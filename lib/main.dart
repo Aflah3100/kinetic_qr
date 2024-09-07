d6c5e94dd2700bf9ab8d72ac0738985d472d17a5
Aflah Sedhique <aflah3100@gmail.com>
2024-09-07 11:25:39 +0530

    Added app demo video

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:kinetic_qr/providers/create_qr_code_screen_provider.dart';
 import 'package:kinetic_qr/providers/home_screen_provider.dart';
